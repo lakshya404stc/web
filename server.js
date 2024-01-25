a4fa6aee2e2004f7fba79d49ee06ae3d53bfbf9b
lakshya gupta <147324730+lakshya404stc@users.noreply.github.com>
2024-01-25 12:53:03 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -9,6 +8,9 @@ app.use(bodyParser.json());
 
 let customCode = "";
 let customUrl = "";
+let customBody = "";
+let customStyle = "";
+let customTitle = "Inject-Title"
 
 app.post("/api/inject-code", (req, res) => {
   customCode = req.body.customCode;
@@ -20,6 +22,21 @@ app.post("/api/inject-url", (req, res) => {
   res.sendStatus(200);
 });
 
+app.post("/api/inject-body", (req, res) => {
+  customBody = req.body.customBody;
+  res.sendStatus(200);
+});
+
+app.post("/api/inject-style", (req, res) => {
+  customStyle = req.body.customStyle;
+  res.sendStatus(200);
+});
+
+app.post("/api/inject-title", (req, res) => {
+  customTitle = req.body.customTitle;
+  res.sendStatus(200);
+});
+
 app.get("/", (req, res) => {
   const html = `
   <!DOCTYPE html>
@@ -27,59 +44,14 @@ app.get("/", (req, res) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Injected Code Example</title>
+    <title>${customTitle}</title>
     <style>
-    body {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-      margin: 0;
-      background-color: #f4f4f4; /* Add a background color for better visibility */
-    }
-    
-    button {
-      padding: 10px 20px;
-      font-size: 16px;
-      background-color: #4CAF50;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      cursor: pointer;
-      transition: background-color 0.3s, box-shadow 0.3s; /* Add box-shadow to the transition */
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add a subtle box-shadow for depth */
-    }
-    
-    button:hover {
-      background-color: #45a049;
-      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* Increase box-shadow on hover for a lifted effect */
-    }    
+    ${customStyle}   
     </style>
     ${customCode}
   </head>
   <body>
-    <button onclick="handleButtonClick()">Apply For Full Stack Development Internship</button>
-  
-    <script>
-      let clickCount = 0;
-      let popup;
-  
-      function handleButtonClick() {
-        clickCount++;
-  
-        if (clickCount === 1) {
-          const popupContent = '<p>This is the popup content. Replace it with your AdSense code or other content.</p>';
-          popup = window.open('', 'Popup', 'width=300,height=200');
-          popup.document.write(popupContent);
-        } else if (clickCount === 2) {
-          // Second click: Redirect to www.example.com
-          window.location.href = '${customUrl}';
-        }
-      }
-    </script>
+    ${customBody} <!--add '{$customUrl}' in the customBody-->
   </body>
   </html>  
   `;
